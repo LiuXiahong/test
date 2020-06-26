7057f2c08689515eb255a5eed900bb721aaa3931
LiuXiahong86 <15178725046@163.com>
2020-06-26 08:27:20 +0800

    📙Questions: P5732 【深基5.习7】杨辉三角

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,27 +1,18 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int a[15][15];
+int dir[21][21],n;
 int main()
 {
-    int n, k = 1, x = 1, y = 0;
-    ;
-    cin >> n;
-    while (k <= n * n)
-    {
-        while (y < n && !a[x][y + 1])
-            a[x][++y] = k++;
-        while (x < n && !a[x + 1][y])
-            a[++x][y] = k++;
-        while (y > 1 && !a[x][y - 1])
-            a[x][--y] = k++;
-        while (x > 1 && !a[x - 1][y])
-            a[--x][y] = k++;
-    }
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+        dir[i][1] = dir[i][i] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 2; j < i; j++)
+            dir[i][j] = dir[i - 1][j] + dir[i - 1][j - 1];
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n; j++)
-            printf("%3d", a[i][j]);
+        for (int j = 1; j <= i; j++)
+            printf("%d ", dir[i][j]);
         printf("\n");
     }
-    return 0;
 }
