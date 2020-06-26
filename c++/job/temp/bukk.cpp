61d1cb0ee1b91c9b2ff292f3fae76ae3a6e5ff56
LiuXiahong86 <15178725046@163.com>
2020-06-26 23:00:07 +0800

    📙Questions: P1614 爱与愁的心痛

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,33 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAX = 30;
-int sq[MAX][MAX][MAX] = {0};
-int a,b,c,n,ans;
+int n,m,t,dir[100001],minn=100000;
 int main()
 {
-	cin>>a>>b>>c>>n;
-	while(n--)
-	{
-		int x1,y1,z1,x2,y2,z2;
-		cin>>x1>>y1>>z1>>x2>>y2>>z2;
-		for(int i = x1;i <= x2;i++)
-        {
-			for(int j = y1;j <= y2;j++)
-            {
-				for(int k = z1;k <= z2;k++) sq[i][j][k] = 1;
-            }
-        }
-	}
-	for(int i = 1;i <= a;i++)
+    cin>>n>>m;
+    for (int i=1;i<=n;i++)
     {
-		for(int j = 1;j <= b;j++)
-        {
-			for(int k = 1;k <= c;k++)
-            {
-				if(sq[i][j][k] == 0) ans++; 
-            }
-        }
+        cin>>dir[i];
     }
-	cout<<ans<<endl; 
-	return 0;
-} 
+    for (int i=1;i<=n-m+1;i++)
+    {
+        for (int j=1;j<=m;j++)
+        {
+            t+=dir[i+j-1];
+        }
+        if (t<minn)
+        {
+            minn=t;
+        }
+        t=0;
+    }
+    cout<<minn;
+    return 0;
+}
