9e2bc3ea2d2991006efae070e7a47fcdceeb978d
LiuXiahong86 <15178725046@163.com>
2020-06-26 22:58:59 +0800

    📙Questions: P5729 【深基5.例7】工艺品制作

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,27 +1,33 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int a[15][15];
+const int MAX = 30;
+int sq[MAX][MAX][MAX] = {0};
+int a,b,c,n,ans;
 int main()
 {
-    int n, k = 1, x = 1, y = 0;
-    ;
-    cin >> n;
-    while (k <= n * n)
+	cin>>a>>b>>c>>n;
+	while(n--)
+	{
+		int x1,y1,z1,x2,y2,z2;
+		cin>>x1>>y1>>z1>>x2>>y2>>z2;
+		for(int i = x1;i <= x2;i++)
+        {
+			for(int j = y1;j <= y2;j++)
+            {
+				for(int k = z1;k <= z2;k++) sq[i][j][k] = 1;
+            }
+        }
+	}
+	for(int i = 1;i <= a;i++)
     {
-        while (y < n && !a[x][y + 1])
-            a[x][++y] = k++;
-        while (x < n && !a[x + 1][y])
-            a[++x][y] = k++;
-        while (y > 1 && !a[x][y - 1])
-            a[x][--y] = k++;
-        while (x > 1 && !a[x - 1][y])
-            a[--x][y] = k++;
+		for(int j = 1;j <= b;j++)
+        {
+			for(int k = 1;k <= c;k++)
+            {
+				if(sq[i][j][k] == 0) ans++; 
+            }
+        }
     }
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-            printf("%3d", a[i][j]);
-        printf("\n");
-    }
-    return 0;
-}
+	cout<<ans<<endl; 
+	return 0;
+} 
