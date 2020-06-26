1ff1170fb467beb75d9589fd5af3c679fbc40e0b
LiuXiahong86 <15178725046@163.com>
2020-06-26 22:58:00 +0800

    📙Questions: P5731 【深基5.习6】蛇形方阵

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,85 +1,27 @@
-/*
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int a[6]={0,2,1,5,7,9},dir[10],sum[10],s;
-int x[3]={2,3,1},y[3]={4,5,3},z[3]={2,-3,5};
+int a[15][15];
 int main()
 {
-    for(int i=0;i<6;i++)
+    int n, k = 1, x = 1, y = 0;
+    ;
+    cin >> n;
+    while (k <= n * n)
     {
-        dir[i]=a[i]-a[i-1];
-        cout<<dir[i]<<' ';
+        while (y < n && !a[x][y + 1])
+            a[x][++y] = k++;
+        while (x < n && !a[x + 1][y])
+            a[++x][y] = k++;
+        while (y > 1 && !a[x][y - 1])
+            a[x][--y] = k++;
+        while (x > 1 && !a[x - 1][y])
+            a[--x][y] = k++;
     }
-    cout<<endl;
-    for(int i=0;i<3;i++)
+    for (int i = 1; i <= n; i++)
     {
-        dir[x[i]]+=z[i];
-        dir[y[i]]-=z[i];
+        for (int j = 1; j <= n; j++)
+            printf("%3d", a[i][j]);
+        printf("\n");
     }
-    for(int i=0;i<6;i++)
-    {
-        s+=dir[i];
-        cout<<s<<' ';
-        sum[i]=sum[i-1]+s;
-    }
-    printf("%d/n%d/n%d/n%d/n",sum[4]-sum[1],sum[5]-sum[2],sum[])
-    return 0;
-}
-*/
-
-/*
-2-4 +3
-1-7 -5
-Q 5
-4-9 +2
-3-8 -7
-Q 3
-Q 8
-6-10 +5
-Q 7
-*/
-#include<bits/stdc++.h>
-using namespace std;
-int a[11]={0,2,1,5,7,9},dir[11],sum[11],s;
-int x[5]={2,1,4,3,6},
-    y[5]={4,7,9,8,10},
-    z[5]={3,-5,2,-7,5};
-int main()
-{
-    for(int i=1;i<=11;i++)
-    {
-        dir[i]=a[i]-a[i-1];
-        //cout<<dir[i]<<' ';
-    }
-    //cout<<endl;
-    dir[x[0]]+=z[0];
-    dir[y[0]]-=z[0];
-    dir[x[1]]+=z[1];
-    dir[y[1]]-=z[1];
-    for(int i=1;i<=11;i++)
-    {
-        s+=dir[i];
-        sum[i]=sum[i-1]+s;
-    }
-    cout<<sum[6]-sum[5]<<' ';
-    dir[x[2]]+=z[2];
-    dir[y[2]]-=z[2];
-    dir[x[3]]+=z[3];
-    dir[y[3]]-=z[3];
-    for(int i=1;i<=11;i++)
-    {
-        s+=dir[i];
-        sum[i]=sum[i-1]+s;
-    }
-    cout<<sum[4]-sum[3]<<' ';
-    cout<<sum[9]-sum[8]<<' ';
-    dir[x[4]]+=z[4];
-    dir[y[4]]-=z[4];
-    for(int i=1;i<=11;i++)
-    {
-        s+=dir[i];
-        sum[i]=sum[i-1]+s;
-    }
-    cout<<sum[10]-sum[9]<<' ';
     return 0;
 }
