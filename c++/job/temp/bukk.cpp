3cd8a97b5801a16551c0a64e94d51b4a68e51146
LiuXiahong86 <15178725046@163.com>
2020-07-04 19:45:17 +0800

    📙Questions: P1276 校门外的树（增强版）

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,25 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m,t,dir[100001],minn=100000;
+int book[100001]={0};
+int n,m,l,r,t;
+int ans1,ans2;
 int main()
 {
     cin>>n>>m;
-    for (int i=1;i<=n;i++)
+    for(int i=0;i<=n;i++)
     {
-        cin>>dir[i];
+        book[i]=1;
     }
-    for (int i=1;i<=n-m+1;i++)
+    for(int i=1;i<=m;i++)
     {
-        for (int j=1;j<=m;j++)
+        cin>>t>>l>>r;
+        if(t==0)
         {
-            t+=dir[i+j-1];
+            for(int j=l;j<=r;j++)
+            {                 
+                
+                if(book[j]==2)
+                {
+                    ans2++;
+                }
+                book[j]=0;
+            }
         }
-        if (t<minn)
+        if(t==1)
         {
-            minn=t;
+            for(int j=l;j<=r;j++)
+            {
+                if(book[j]==0)
+                {
+                    book[j]=2;
+                }
+            }
         }
-        t=0;
     }
-    cout<<minn;
-    return 0;
+    for(int i=0;i<=n;i++)
+        if(book[i]==2)
+            ans1++;
+    cout<<ans1<<endl<<ans2;
 }
+    //校门外马路上本来从编号0到L，每一编号的位置都有1棵树。
+    //有砍树者每次从编号A到B处连续砍掉每1棵树，就连树苗也不放过（记 0 A B ，含A和B）；
+    //幸运的是还有植树者每次从编号C到D 中凡是空穴（树被砍且还没种上树苗或树苗又被砍掉）
+    //的地方都补种上树苗（记 1 C D，含C和D）；
+    //问最终校门外留下的树苗多少棵？植树者种上又被砍掉的树苗有多少棵？
