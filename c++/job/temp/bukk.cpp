5f1420cd7675dd884c2a421637a9639fbab92488
LiuXiahong86 <15178725046@163.com>
2020-07-09 12:14:04 +0800

    📙Questions: P1015 回文数

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,48 +1,42 @@
+//p1015
 #include<bits/stdc++.h>
-using namespace std;
-int book[100001]={0};
-int n,m,l,r,t;
-int ans1,ans2;
+const int S=303;
+int n,a[S],l;
+char c[S],d[S];
+inline void add()
+{
+	for (int i=0;i<l;++i)
+		d[l-i-1]=c[i];
+	l+=2;
+	for (int i=0;i<l;++i)
+	{
+		c[i]+=d[i];
+		if (c[i]>=n) c[i+1]++,c[i]-=n;
+	}
+	while (!c[l-1]) --l;
+}
+inline bool pd()
+{
+	for (int i=0;i<l;++i)
+		if (c[i]!=c[l-1-i]) return false;
+	return true;
+}
 int main()
 {
-    cin>>n>>m;
-    for(int i=0;i<=n;i++)
-    {
-        book[i]=1;
-    }
-    for(int i=1;i<=m;i++)
-    {
-        cin>>t>>l>>r;
-        if(t==0)
-        {
-            for(int j=l;j<=r;j++)
-            {                 
-                
-                if(book[j]==2)
-                {
-                    ans2++;
-                }
-                book[j]=0;
-            }
-        }
-        if(t==1)
-        {
-            for(int j=l;j<=r;j++)
-            {
-                if(book[j]==0)
-                {
-                    book[j]=2;
-                }
-            }
-        }
-    }
-    for(int i=0;i<=n;i++)
-        if(book[i]==2)
-            ans1++;
-    cout<<ans1<<endl<<ans2;
+	scanf("%d%s",&n,c);l=strlen(c);
+	for (int i=0;i<l;++i)
+	{
+		if (c[i]>='0' && c[i]<='9') c[i]-='0';
+		else c[i]=c[i]-'A'+10;
+	}
+	int step=0;
+	while (!pd())
+	{
+		++step;
+		if (step>30) break;
+		add();
+	}
+	if (step<=30) printf("STEP=%d\n",step);
+	else puts("Impossible!");
+	return 0;
 }
-    //校门外马路上本来从编号0到L，每一编号的位置都有1棵树。
-    //有砍树者每次从编号A到B处连续砍掉每1棵树，就连树苗也不放过（记 0 A B ，含A和B）；
-    //幸运的是还有植树者每次从编号C到D 中凡是空穴（树被砍且还没种上树苗或树苗又被砍掉）
-    //的地方都补种上树苗（记 1 C D，含C和D）；
-    //问最终校门外留下的树苗多少棵？植树者种上又被砍掉的树苗有多少棵？
