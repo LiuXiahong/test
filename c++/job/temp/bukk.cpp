362eebeb13cd228b263b1f86ec02dfee01d508d1
LiuXiahong86 <15178725046@163.com>
2020-07-09 12:42:25 +0800

    📙Questions: P1147 连续自然数和

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,42 +1,15 @@
-//p1015
-#include<bits/stdc++.h>
-const int S=303;
-int n,a[S],l;
-char c[S],d[S];
-inline void add()
-{
-	for (int i=0;i<l;++i)
-		d[l-i-1]=c[i];
-	l+=2;
-	for (int i=0;i<l;++i)
-	{
-		c[i]+=d[i];
-		if (c[i]>=n) c[i+1]++,c[i]-=n;
-	}
-	while (!c[l-1]) --l;
-}
-inline bool pd()
-{
-	for (int i=0;i<l;++i)
-		if (c[i]!=c[l-1-i]) return false;
-	return true;
-}
+//p1147
+#include <bits/stdc++.h>
+using namespace std;
+int m;
 int main()
 {
-	scanf("%d%s",&n,c);l=strlen(c);
-	for (int i=0;i<l;++i)
-	{
-		if (c[i]>='0' && c[i]<='9') c[i]-='0';
-		else c[i]=c[i]-'A'+10;
-	}
-	int step=0;
-	while (!pd())
-	{
-		++step;
-		if (step>30) break;
-		add();
-	}
-	if (step<=30) printf("STEP=%d\n",step);
-	else puts("Impossible!");
+	cin >> m;
+	for (int k1 = sqrt(2 * m); k1 > 1; k1--)
+		if (2 * m % k1 == 0 && (k1 + 2 * m / k1) % 2)
+		{
+			int k2 = 2 * m / k1;
+			cout << (k2 - k1 + 1) / 2 << " " << (k1 + k2 - 1) / 2 << endl;
+		}
 	return 0;
 }
