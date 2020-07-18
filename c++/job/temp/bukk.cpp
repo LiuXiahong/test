d89215eaa9a353f3d713da84a88fcba8d50a229e
LiuXiahong86 <15178725046@163.com>
2020-07-18 21:34:00 +0800

    📙Questions: P1244 [NOI2000]青蛙过河

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,15 +1,10 @@
-//p1147
+//P1244
 #include <bits/stdc++.h>
 using namespace std;
-int m;
+int h,k;
 int main()
 {
-	cin >> m;
-	for (int k1 = sqrt(2 * m); k1 > 1; k1--)
-		if (2 * m % k1 == 0 && (k1 + 2 * m / k1) % 2)
-		{
-			int k2 = 2 * m / k1;
-			cout << (k2 - k1 + 1) / 2 << " " << (k1 + k2 - 1) / 2 << endl;
-		}
-	return 0;
+    cin>>h>>k;
+    cout<<(k+1)*(1<<h);
+    return 0;
 }
