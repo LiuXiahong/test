842b43f5751e7bc5d5521efd2852f8d478047ad2
LiuXiahong86 <15178725046@163.com>
2020-07-19 22:14:47 +0800

    📘Class: 拓扑排序练习

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,12 +1,9 @@
-//P4017
 #include<bits/stdc++.h>
 using namespace std;
-const int MAX=80112002;
-vector<int> v[5001];
-queue<int> q;
-int in[50001],out[50001],dis[5001];
+vector<int> v[5001];//存储有向无环图
+queue<int> q;//bfs队列
+int in[50001];//存储每一个节点的入度
 int a,b,n,m;
-long long ans;
 int main()
 {
     cin>>n>>m;
@@ -14,38 +11,28 @@ int main()
     {
         cin>>a>>b;
         v[a].push_back(b);
-        in[b]++;
-        out[a]++;
+        in[b]++;//b节点的入度+1
     }
     for(int i=1;i<=n;i++)
     {
-        if(in[i]==0)
+        if(in[i]==0)//如果有一个节点的入度为0(起点)
         {
-            q.push(i);
-            dis[i]=1;
+            q.push(i);//将此节点压入队列作为起点
         }
     }
+    //bfs
     while(q.size())
     {
-        int t=q.front();
-        q.pop();
-        for(int i=0;i<v[t].size();i++)
+        int t=q.front();//取出头部节点
+        q.pop();//弹出头部节点
+        for(int i=0;i<v[t].size();i++)//遍历
         {
             int node_t=v[t][i];
-            in[node_t]--;
-            dis[node_t]=(dis[node_t]+dis[t])%MAX;
-            if(in[node_t]==0)
+            in[node_t]--;//将此节点的入度-1,因为已经遍历
+            if(in[node_t]==0)//如果此节点入度为0
             {
-                q.push(node_t);
+                q.push(node_t);//压入队列,作为下一次起点
             }
         }
     }
-    for(int i=1;i<=n;i++)
-    {
-        if(out[i]==0)
-        {
-            ans=(ans+dis[i])%MAX;
-        }
-    }
-    cout<<ans%MAX;
 }
