e5585b7195216b189a0fc16716459724498438cc
LiuXiahong86 <15178725046@163.com>
2020-07-22 18:28:18 +0800

    📙Questions: P1090 合并果子

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,10 +1,40 @@
-//P5711 【深基3.例3】闰年判断
+/* 
+//P5712 【深基3.例4】Apples
 #include<bits/stdc++.h>
 using namespace std;
-int n;
+int n,a,b;
 int main()
 {
 	cin>>n;
-	printf("%d",(n%4==0)&&(n%100!=0)||(n%400==0));
+	a=5*n;
+	b=11+3*n;
+	if(a>b)
+	return 0;
+}
+*/
+//P1090 合并果子 / [USACO06NOV] Fence Repair G
+#include<bits/stdc++.h>
+using namespace std;
+priority_queue<int, vector<int>, greater<int> > p;
+int n,t;
+int ans,a,b;
+int main()
+{
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>t;
+		p.push(t);
+	}
+	while(p.size()>=2)
+	{
+		a=p.top();
+		p.pop();
+		b=p.top();
+		p.pop();
+		ans+=a+b;
+		p.push(a+b);
+	}
+	cout<<ans;
 	return 0;
 }
