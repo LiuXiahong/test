0aa20eca93876fa5e0dc94b0716216c11ccac8b6
LiuXiahong86 <15178725046@163.com>
2020-07-22 14:53:36 +0800

    📙Questions: P2181 对角线

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,37 +1,11 @@
-//P2419 [USACO08JAN]Cow Contest S
+//P2181 对角线
 #include<bits/stdc++.h>
 using namespace std;
-int a,b,n,m,t,f[101][101];
-int ans;
+unsigned long long n,ans;
 int main()
 {
-    cin>>n>>m;
-    for(int i=1;i<=m;i++)
-	{
-        cin>>a>>b;
-        f[a][b]=1;
-    }
-    for(int k=1;k<=n;k++)
-	{
-        for(int i=1;i<=n;i++)
-		{
-            for(int j=1;j<=n;j++)
-			{
-              f[i][j]=f[i][j]|f[i][k]&f[k][j];
-			}
-		}
-	}
-    for(int i=1;i<=n;i++)
-	{
-        t=1;
-        for(int j=1;j<=n;j++)
-        if(i==j)
-			continue;
-		else
-		{
-			t=t&(f[i][j]|f[j][i]);
-		} 
-		ans+=t;
-    }
-    
+    scanf("%lld",&n);
+    ans=n * (n-1) / 2 * (n-2) / 3 * (n-3) / 4;
+    printf("%lld\n",ans);
+    return 0;
 }
