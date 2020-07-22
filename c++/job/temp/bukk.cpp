9ddc254245433205e6644a3fb0b5ac2b401a95b8
LiuXiahong86 <15178725046@163.com>
2020-07-22 19:51:44 +0800

    📙Questions: P5715 【深基3.例8】三位数排序

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -59,25 +59,14 @@ int main()
 	return 0;
 }
 */
-//P5714 【深基3.例7】肥胖问题
+//P5715	【深基3.例8】三位数排序
 #include<bits/stdc++.h>
 using namespace std;
-double m,h,bmi;
+int a[3];
 int main()
 {
-	cin>>m>>h>>bmi;
-	bmi=m/(h*h);
-	if(bmi>=24)
-	{
-		cout<<bmi<<endl<<"Overweight";
-	}
-	if(bmi>=18.5&&bmi<24)
-	{
-		cout<<"Normal";
-	}
-	if(bmi<18.5)
-	{
-		cout<<"Underweight";
-	}
+	cin>>a[0]>>a[1]>>a[2];
+	sort(a,a+3);
+	cout<<a[0]<<' '<<a[1]<<' '<<a[2];
 	return 0;
 }
