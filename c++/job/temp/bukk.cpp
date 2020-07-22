627bb6c666e732d5adbba7f5ad7204e2ca7840bd
LiuXiahong86 <15178725046@163.com>
2020-07-22 17:15:32 +0800

    📙Questions: P5711 【深基3.例3】闰年判断

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,13 +1,10 @@
-//P5710 【深基3.例2】数的性质
+//P5711 【深基3.例3】闰年判断
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-bool a,b;
 int main()
 {
 	cin>>n;
-	a=(n%2==0);
-	b=n>4&&n<=12;
-	cout << (a && b) << " " << (a || b) << " " << ((a == 1 && b == 0) || (a == 0 && b == 1)) << " " << (!a && !b) << endl;
+	printf("%d",(n%4==0)&&(n%100!=0)||(n%400==0));
 	return 0;
 }
