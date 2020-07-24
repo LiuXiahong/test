952afe597758ec51cc0b70c406f5d490c3bd7c16
LiuXiahong86 <15178725046@163.com>
2020-07-24 15:33:25 +0800

    📙Questions: P5723 【深基4.例13】质数口袋

--- c++/job/temp/bukk.cpp
+++ c++/job/temp/bukk.cpp
@@ -1,15 +1,47 @@
-//P5720 【深基4.例4】一尺之棰
-#include<bits/stdc++.h>
+//P5723 【深基4.例13】质数口袋
+#include <bits/stdc++.h>
 using namespace std;
-int n,i;
+int n, t;
+long long sum = 0;
+int f(int x)
+{
+	for (int i = 2; i * i <= x; i++)
+	{
+		if (x % i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
 int main()
 {
-	cin>>n;
-	while(n!=1)
+	scanf("%d", &n);
+	if (n < 2)
 	{
-		i++;
-		n/=2;
+		printf("0\n");
+		return 0;
 	}
-	cout<<i+1;
+	else if (n == 2)
+	{
+		printf("2\n1\n");
+		return 0;
+	}
+	for (int i = 2; i <= n; i++)
+	{
+		if (i % 2 == 0 && i != 2)
+			continue;
+		if (sum + i > n)
+		{
+			printf("%d\n", t);
+			return 0;
+		}
+		if (f(i))
+		{
+			printf("%d\n", i);
+			sum += i;
+			t++;
+		}
+	}
 	return 0;
 }
